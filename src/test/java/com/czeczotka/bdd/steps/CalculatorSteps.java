6692c3fd0cb5a2ca2607b5f0ca5aa385a575ada9
mqmetry <119501023+mqmetry@users.noreply.github.com>
2024-08-30 16:08:11 +0530

    Update CalculatorSteps.java

--- src/test/java/com/czeczotka/bdd/steps/CalculatorSteps.java
+++ src/test/java/com/czeczotka/bdd/steps/CalculatorSteps.java
@@ -2,12 +2,12 @@ package com.czeczotka.bdd.steps;
 
 import com.czeczotka.bdd.calculator.Calculator;
 
-import cucumber.api.Scenario;
-import cucumber.api.java.After;
-import cucumber.api.java.Before;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+import io.cucumber.java.Scenario;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
