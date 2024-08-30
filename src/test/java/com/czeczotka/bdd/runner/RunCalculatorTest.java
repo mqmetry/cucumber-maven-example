f87e5a8ef055ba78d990db0c42f2671bb3150081
mqmetry <119501023+mqmetry@users.noreply.github.com>
2024-08-30 14:36:12 +0530

    Update RunCalculatorTest.java

--- src/test/java/com/czeczotka/bdd/runner/RunCalculatorTest.java
+++ src/test/java/com/czeczotka/bdd/runner/RunCalculatorTest.java
@@ -8,8 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
 		  features = "classpath:cucumber/calculator.feature" ,
 		  glue = "com.czeczotka.bdd.steps",
-		monochrome = true,
-format = "json:target/cucumber-json-report.json"
+		  format = {"json:target/cucumber-json-report.json"}
 
 )
 public class RunCalculatorTest {
